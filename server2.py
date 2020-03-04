eb1b11f4eb1bfa69257a9af52870d7f44561da1f
Roman A <andreenkorv@yandex.ru>
2020-03-04 15:37:20 +0300

    два города

--- server2.py
+++ server2.py
@@ -11,8 +11,10 @@ def index():
     page_title = "Новости Python"
     city_name = "Norilsk,Russia"
     weather = weather_by_city(city_name)
+    city_name2 = "Saint-Petersburg,Russia"
+    weather2 = weather_by_city(city_name2)
     news_list = get_python_news()
-    return render_template('bootstrap.html', page_title=page_title, weather=weather, city_name=city_name, news_list=news_list)
+    return render_template('bootstrap.html', page_title=page_title, weather=weather, city_name=city_name, weather2=weather2, city_name2=city_name2, news_list=news_list)
 
 
 if __name__ == "__main__":
